904d0f6140485428ef29f5abc77be4fe16afc1f6
Francesco Rossi <mail.francesco.rossi@gmail.com>
2016-11-06 16:03:41 -0800

    Adding config.py and an dummy filter for throttle

--- NeuralNet/filemash.py
+++ NeuralNet/filemash.py
@@ -24,6 +24,7 @@ import cv2
 import Warp
 import numpy as np
 import manual_throttle_map
+import config
 
 # Parse args.
 args = docopt(__doc__)
@@ -148,6 +149,28 @@ if __name__ == '__main__':
             all_groundtruth_throttle.append(log_throttle)
         if ((int(s[1]) % 1024) == 1023):
             print s[1] + "    " + str(steer) + "    " + str(log_steer) + "    " + str(log_throttle)
+
+    if config.use_median_filter_throttle:
+        import numpy as np
+        
+        def medfilt (x, k):
+            """Apply a length-k median filter to a 1D array x.
+            Boundaries are extended by repeating endpoints.
+            """
+            assert k % 2 == 1, "Median filter length must be odd."
+            assert x.ndim == 1, "Input must be one-dimensional."
+            k2 = (k - 1) // 2
+            y = np.zeros ((len (x), k), dtype=x.dtype)
+            y[:,k2] = x
+            for i in range (k2):
+                j = k2 - i
+                y[j:,i] = x[:-j]
+                y[:j,i] = x[0]
+                y[:-j,-(i+1)] = x[j:]
+                y[-j:,-(i+1)] = x[-1]
+            return np.median (y, axis=1)
+
+        all_groundtruth_throttle = medfilt(all_groundtruth_throttle, 5)
 
     # Save data.
     outpath = os.path.expanduser(args['--outdir'])
