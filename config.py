07e0b07d8e5d3ffbe15c5575007c970d42c129ce
Francesco Rossi <mail.francesco.rossi@gmail.com>
2016-11-06 16:03:53 -0800

    Adding config.py and an dummy filter for throttle

---
+++ config.py
@@ -0,0 +1,13 @@
+
+# This file to contain all the config variables shared across modules
+
+use_median_filter_throttle = True
+#camera_id = 0
+
+
+# Create local_config.py to have local settings
+try :
+	from local_config import *
+except:
+	pass
+
