769f05fbc145a8ae454a926f9ac7629cd68e43ae
Francesco Rossi <mail.francesco.rossi@gmail.com>
2016-11-06 16:52:06 -0800

    changing default options

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 
 # This file to contain all the config variables shared across modules
 
-use_median_filter_throttle = True
 use_throttle_manual_map = True
-split_softmax = True
+use_median_filter_throttle = False
+split_softmax = False
 
 #camera_id = 0
 
