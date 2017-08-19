e5e916607e73167b638316b5850e6c608af85e91
otaviogood <otaviogood@yahoo.com>
2017-08-19 12:48:14 -0700

    Simulator of Oakland warehouse racetrack in Unity. First checkin, no lighting.

---
+++ warehouse_sim/Assets/Scripts/ExternalPipe.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class ExternalPipe : MonoBehaviour
+{
+    public string remoteAddress = "127.0.0.1";
+    public int remotePort = 12345;
+    public CarController car;
+
+    private Socket socket;
+    private bool wasClosed = false;
+
+    // A circular buffer of bytes coming from the other end
+    private const int INCOMING_MESSAGE_SIZE = 8;
+    private byte[] incomingMessage = new byte[INCOMING_MESSAGE_SIZE];
+    private int incomingOffset = 0;
+
+    void Start()
+    {
+        return;
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        socket.NoDelay = true; // Is this a good idea?
+        socket.Connect(remoteAddress, remotePort);
+    }
+
+    void Update()
+    {
+        return;
+        if (wasClosed)
+        {
+            return;
+        }
+
+        int pendingBytes = socket.Available;
+        if (pendingBytes == 0)
+        {
+            return;
+        }
+
+        byte[] bytes = new byte[pendingBytes];
+        int count = 0;
+
+        try
+        {
+            count = socket.Receive(bytes);
+        }
+        catch (SocketException)
+        {
+            Debug.Log("ExternalPipe: Receive threw SocketException");
+            wasClosed = true;
+            return;
+        }
+
+        if (count == 0)
+        {
+            Debug.Log("ExternalPipe: Disconnected");
+            wasClosed = true;
+            return;
+        }
+
+        for (int i = 0; i < count; i++)
+        {
+            incomingMessage[incomingOffset++] = bytes[i];
+
+            // Tell the handler when we have a complete message
+            if (incomingOffset == INCOMING_MESSAGE_SIZE)
+            {
+                if (car != null)
+                {
+                    car.HandleExternalMessage(incomingMessage);
+                }
+
+                // Wrap back around for the next message
+                incomingOffset = 0;
+            }
+        }
+    }
+
+    // Returns false if sending failed
+    public bool Send(byte[] bytes)
+    {
+        if (wasClosed)
+        {
+            return false;
+        }
+
+        try
+        {
+            socket.Send(bytes);
+        }
+        catch (SocketException)
+        {
+            Debug.Log("ExternalPipe: Send threw SocketException");
+            wasClosed = true;
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ warehouse_sim/Assets/Scripts/RandomScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomScript : MonoBehaviour
+{
+    public GameObject capcam;
+    public GameObject maincam;
+
+    private Vector3 camAngles;
+    //private Material skybox;
+    //private float exposure;
+
+    private Color RandomColorDelta(float scale)
+    {
+        return new Color(Random.Range(-scale, scale), Random.Range(-scale, scale), Random.Range(-scale, scale), 0.0f);
+    }
+
+    public void Randomize(int seed, float scale = 1.0f)
+    {
+        Random.InitState(seed);
+
+        // Change properties of lighting and sky.
+        //RenderSettings.skybox.SetFloat("_Exposure", exposure + Random.Range(-0.5f, 2.0f) * scale);
+
+        capcam.transform.localEulerAngles = camAngles + new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f)) * scale;
+        maincam.transform.localEulerAngles = camAngles + new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f)) * scale;
+    }
+
+    void SaveStartState()
+    {
+        //exposure = RenderSettings.skybox.GetFloat("_Exposure");
+
+        if (capcam == null) capcam = GameObject.Find("/car_root/CameraCapture");
+        if (maincam == null) maincam = GameObject.Find("/car_root/CameraMain");
+        camAngles = capcam.transform.localEulerAngles;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        SaveStartState();
+        Randomize(0, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //		if ((Time.frameCount % 15) == 0)
+        //			Randomize(Random.Range(0, 32000));
+    }
+}
