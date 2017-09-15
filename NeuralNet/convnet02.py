hack-fixed RC packet drop bug. Cleaned up softmax code. Now regression.
We have a bad radio control, so I filter out the dropped packets or
whatever in filemash.py. I got rid of all softmax output from the neural
net, so now I'm committed to regression output. It looks smooth. I fine
tuned the model and the preprocessing of input images. I cleaned up the
html debug visualization file to work with just regression, but also
have throttle visualization. I fixed plot_vs_time so it works.

NeuralNet/convnet02.py
@@ -68,8 +68,8 @@ train_data = TrainingData.fromfilename("train", args['--indir'])
 test_data = TrainingData.fromfilename("test", args['--indir'])
 print("In dir: {}".format(args["--indir"]))
 
-numTest = 2000
-skipTest = 4
+numTest = 8000
+skipTest = 1
 if config.running_on_laptop:
     numTest = 384 * 2
     skipTest = 4
@@ -93,85 +93,76 @@ allAccuracyTest = []
 print '\n\n-----------------------'
 print 'saving output data to %s' % output_path
 print 'training on %s images' % train_data.NumSamples()
+print 'testing on %s images' % test_data.NumSamples()
 print '-----------------------\n'
 
 random.seed(111)
 iteration = 0
 accuracy_check_iterations = []
 sliding_window = []
-sliding_window_size = 64
+sliding_window_size = 16
 sliding_window_graph = []
 
 #Headers for the console debug output
-debug_header_list = ['Iteration', 'Test Accuracy', 'Test Throttle Accuracy', 'Sliding Average', 'Elapsed Time', 'steer regression']
+debug_header_list = ['Iteration', 'Elapsed Time', 'Sliding Average', 'steer regression', 'throttle regression']
 print '%s' % ' | '.join(map(str, debug_header_list))
 
 #Vars to calculate deltas between iterations
-prev_acc = 0
-prev_throttle_acc = 0
 prev_sliding_window = 0
 prev_squared_diff = 0.0
+prev_squared_diff_throttle = 0.0
 
 merged_summaries = tf.summary.merge_all()
 train_writer = tf.summary.FileWriter('train', sess.graph)
 
 start_time = time.time()
-while iteration < 100*128:
+while iteration < 1000*128:
     batch = train_data.GenRandomBatch()
     train_feed_dict = batch.FeedDict(net_model, 0.6)
-    # [summary_str, _, train_steer_acc] = sess.run(
-    #     [merged_summaries, net_model.train_step, net_model.steering_accuracy], feed_dict=train_feed_dict)
-    [summary_str, _, train_steer_acc, squared_diff] = sess.run(
-        [merged_summaries, net_model.train_step, net_model.steering_accuracy, net_model.squared_diff], feed_dict = train_feed_dict)
+    [summary_str, _, train_steer_diff] = sess.run(
+        [merged_summaries, net_model.train_step, net_model.squared_diff], feed_dict = train_feed_dict)
     train_writer.add_summary(summary_str, iteration)
 
 
     # Check the accuracy occasionally.
-    if ((iteration % 64) == 63):
-        [acc,
-         throttle_acc,
-         results_steering,
-         results_throttle,
-         results_steering_softmax,
-         results_throttle_softmax,
-         results_steering_regress,
+    if ((iteration % 128) == 127):
+        [results_steering_regress,
          results_throttle_regress,
          results_squared_diff,
-         ] = sess.run([net_model.steering_accuracy,
-                       net_model.throttle_accuracy,
-                       net_model.steering_pred,
-                       net_model.throttle_pred,
-                       net_model.steering_softmax,
-                       net_model.throttle_softmax,
-                       net_model.steering_regress_result,
+         results_squared_diff_throttle,
+         results_loss,
+         results_regularizers,
+         ] = sess.run([net_model.steering_regress_result,
                        net_model.throttle_regress_result,
                        net_model.squared_diff,
+                       net_model.squared_diff_throttle,
+                       net_model.loss,
+                       net_model.regularizers,
                        ], feed_dict=test_data.FeedDict(net_model))
 
-        allAccuracyTest.append(acc)
-        sliding_window.append(acc)
+        allAccuracyTest.append(results_squared_diff)
+        sliding_window.append(results_squared_diff)
         if len(sliding_window) > sliding_window_size: sliding_window = sliding_window[1:]
         sliding_window_graph.append(sum(sliding_window)/len(sliding_window))
 
         # print results_steering
         # print results_throttle
         html_output.write_html(
-            output_path, test_data, results_steering, results_throttle, tf.get_default_graph(),
-            sess, results_steering_softmax, results_throttle_softmax, results_steering_regress, results_throttle_regress, net_model)
+            output_path, test_data, tf.get_default_graph(),
+            sess, results_steering_regress, results_throttle_regress, net_model)
 
         accuracy_check_iterations.append(iteration)
-        allAccuracyTrain.append(train_steer_acc)
-
+        allAccuracyTrain.append(train_steer_diff)
         plt.plot(accuracy_check_iterations, allAccuracyTrain, 'bo')
         plt.plot(accuracy_check_iterations, allAccuracyTrain, 'b-')
         plt.plot(accuracy_check_iterations, allAccuracyTest, 'ro')
         plt.plot(accuracy_check_iterations, allAccuracyTest, 'r-')
         plt.plot(accuracy_check_iterations, sliding_window_graph, 'g-')
         axes = plt.gca()
-        axes.set_ylim([0, 30.05])
+        axes.set_ylim([0, 1000.05])
         plt.title("training (blue), test (red), avg " + str(round(sliding_window_graph[-1], 5)) + "  /  " + str(len(sliding_window)))
         plt.xlabel('iteration')
-        plt.ylabel('accuracy')
+        plt.ylabel('diff squared')
         plt.savefig(os.path.join(output_path, "progress.png"))
 
         # Save the model.
@@ -180,18 +171,10 @@ while iteration < 100*128:
 
         # put the print after writing everything so it indicates things have been written.
         debug_iteration = format(iteration, '^10')
+        debug_loss = format(results_loss, '^16')
+        debug_regularizers = format(results_regularizers, '^16')
 
-        #Format accuracy
-        debug_acc = generate_color_text(prev_acc, acc)
-        debug_acc = format(debug_acc, '^24')
-        prev_acc = acc
-
-        #Format throttle accuracy
-        debug_throttle_acc = generate_color_text(prev_throttle_acc, throttle_acc)
-        debug_throttle_acc = format(debug_throttle_acc, '^33')
-        prev_throttle_acc = throttle_acc
-
-        #Format sliding window 
+        #Format sliding window
         debug_sliding_window = generate_color_text(prev_sliding_window, sliding_window_graph[-1])
         debug_sliding_window = format(debug_sliding_window, '^25')
         prev_sliding_window = sliding_window_graph[-1]
@@ -200,13 +183,17 @@ while iteration < 100*128:
         debug_sqr_acc = generate_color_text(prev_squared_diff, results_squared_diff)
         debug_sqr_acc = format(debug_sqr_acc, '^33')
         prev_squared_diff = results_squared_diff
+        # Format throttle regression accuracy
+        debug_sqr_acc_throttle = generate_color_text(prev_squared_diff_throttle, results_squared_diff_throttle)
+        debug_sqr_acc_throttle = format(debug_sqr_acc_throttle, '^33')
+        prev_squared_diff_throttle = results_squared_diff_throttle
 
         elapsed_time = format(format(time.time() - start_time, ".2f"), '^17')
 
         # all_pulse_entropy = np.sum(np.multiply(results_pulse_softmax, np.log(np.reciprocal(results_pulse_softmax))))
 
         #Print everything
-        print("%s %s %s %s %s %s" % (debug_iteration, debug_acc, debug_throttle_acc, debug_sliding_window, elapsed_time, debug_sqr_acc))
+        print("%s %s %s %s %s %s %s" % (debug_iteration, elapsed_time, debug_sliding_window, debug_sqr_acc, debug_sqr_acc_throttle, debug_loss, debug_regularizers))
         start_time = time.time()
 
     # Increment.