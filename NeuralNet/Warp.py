hack-fixed RC packet drop bug. Cleaned up softmax code. Now regression.
We have a bad radio control, so I filter out the dropped packets or
whatever in filemash.py. I got rid of all softmax output from the neural
net, so now I'm committed to regression output. It looks smooth. I fine
tuned the model and the preprocessing of input images. I cleaned up the
html debug visualization file to work with just regression, but also
have throttle visualization. I fixed plot_vs_time so it works.

NeuralNet/Warp.py
@@ -41,12 +41,12 @@ def RandRects(source):
     w = source.width
     h = source.height
     draw = ImageDraw.Draw(source)
-    for i in xrange(6):
+    for i in xrange(random.randint(0, 32)):
         rs = np.random.rand(4)
         rc = np.random.randn(3) * 0.3
         rc = np.clip(rc, -1.0, 1.0) * 127 + 127
         draw.rectangle([(rs[0]*1.4-0.2)*w, (rs[1]*1.4-0.2)*h, (rs[0]*1.4-0.2)*w + rs[2]*40, (rs[1]*1.4-0.2)*h + rs[3]*40], fill=(int(rc[0]), int(rc[1]), int(rc[2])))
-    # draw.rectangle([0, 80, 128, 128], fill=(0, 32, 0))
+    # draw.rectangle([0, 0, 128, 64], fill=(128, 128, 128))
 
 
 def WhiteUnbalance(source):

NeuralNet/convnetshared1.py
@@ -32,7 +32,6 @@ def max_pool_2x2(x):
 
 class NNModel:
     # static class vars
-    max_log_outs = 15
     width = 128
     height = 128
     widthD2 = width / 2
@@ -58,12 +57,8 @@ class NNModel:
     def __init__(self):
         self.in_image = tf.placeholder(tf.float32, shape=[None, NNModel.width * NNModel.height * NNModel.img_channels], name='in_image')
         self.in_speed = tf.placeholder(tf.float32, shape=[None], name='in_speed')
-        self.steering_ = tf.placeholder(tf.float32, shape=[None, NNModel.max_log_outs], name='steering_')
         self.steering_regress_ = tf.placeholder(tf.float32, shape=[None], name='steering_regress_')
         self.throttle_regress_ = tf.placeholder(tf.float32, shape=[None], name='throttle_regress_')
-        self.throttle_ = tf.placeholder(tf.float32, shape=[None, NNModel.max_log_outs], name='throttle_')
-        # self.lat_ = tf.placeholder(tf.float32, shape=[None, config.latlon_buckets], name='lat_')
-        # self.lon_ = tf.placeholder(tf.float32, shape=[None, config.latlon_buckets], name='lon_')
 
         self.keep_prob = tf.placeholder(tf.float32)
         self.train_mode = tf.placeholder(tf.float32)
@@ -109,200 +104,40 @@ class NNModel:
         h_fc2 = tf.nn.relu(tf.matmul(h_fc1_drop_concat, W_fc2) + b_fc2)
 
         # num_outputs = 2 * NNModel.max_log_outs + 2 * config.latlon_buckets + 1 + config.latlon_buckets * config.latlon_buckets
-        num_outputs = 2 * NNModel.max_log_outs + 1 + 1
+        num_outputs = 1 + 1
         W_fc4 = weight_variable([NNModel.fc2_num_outs, num_outputs], NNModel.fc2_num_outs, num_outputs, name='W_fc4', coll='main')
         b_fc4 = bias_variable([num_outputs], name='b_fc4', coll='main')
 
         final_activations = tf.matmul(h_fc2, W_fc4) + b_fc4
-        slice_a = NNModel.max_log_outs
-        self.steering_softmax = tf.nn.softmax(final_activations[:, :slice_a])
-        slice_b = slice_a + NNModel.max_log_outs
-        self.throttle_softmax = tf.nn.softmax(final_activations[:, slice_a:slice_b])
-        # slice_c = slice_b + config.latlon_buckets
-        # self.lat_softmax = tf.nn.softmax(final_activations[:, slice_b:slice_c])
-        # slice_d = slice_c + config.latlon_buckets
-        # self.lon_softmax = tf.nn.softmax(final_activations[:, slice_c:slice_d])
-        slice_e = slice_b + 1
-        self.steering_regress_result = tf.reshape(final_activations[:, slice_b:slice_e], [-1])
-        slice_f = slice_e + 1
-        self.throttle_regress_result = tf.reshape(final_activations[:, slice_e:slice_f], [-1])
+        slice_a = 1
+        self.steering_regress_result = tf.reshape(final_activations[:, 0:slice_a], [-1])
+        slice_b = slice_a + 1
+        self.throttle_regress_result = tf.reshape(final_activations[:, slice_a:slice_b], [-1])
 
-        # cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
-        # http://stackoverflow.com/questions/33712178/tensorflow-nan-bug
-
-        steering_cross_entropy = -tf.reduce_mean(self.steering_ * tf.log(tf.clip_by_value(self.steering_softmax, 1e-10, 1.0)))
-        throttle_cross_entropy = -tf.reduce_mean(self.throttle_ * tf.log(tf.clip_by_value(self.throttle_softmax, 1e-10, 1.0)))
-        # lat_cross_entropy = -tf.reduce_mean(self.lat_ * tf.log(tf.clip_by_value(self.lat_softmax, 1e-10, 1.0)))
-        # lon_cross_entropy = -tf.reduce_mean(self.lon_ * tf.log(tf.clip_by_value(self.lon_softmax, 1e-10, 1.0)))
 
         self.squared_diff = tf.reduce_mean(tf.squared_difference(self.steering_regress_result, self.steering_regress_))
         self.squared_diff_throttle = tf.reduce_mean(tf.squared_difference(self.throttle_regress_result, self.throttle_regress_))
-        # cross_entropy = (steering_cross_entropy + (throttle_cross_entropy + lat_cross_entropy + lon_cross_entropy)) / 4.0
-        # cross_entropy = (steering_cross_entropy + (throttle_cross_entropy + latlon_cross_entropy + lat_cross_entropy + lon_cross_entropy)) / 5.0
-        cross_entropy = (throttle_cross_entropy + steering_cross_entropy) / 2.0
-        tf.summary.scalar('cross_entropy', cross_entropy)
 
         # L2 regularization
-        regularizers = (tf.nn.l2_loss(W_conv1) + tf.nn.l2_loss(b_conv1) +
-                        tf.nn.l2_loss(W_conv2) + tf.nn.l2_loss(b_conv2) +
-                        tf.nn.l2_loss(W_conv3) + tf.nn.l2_loss(b_conv3) +
-                        tf.nn.l2_loss(W_conv4) + tf.nn.l2_loss(b_conv4) +
-                        tf.nn.l2_loss(W_fc1) + tf.nn.l2_loss(b_fc1) +
-                        tf.nn.l2_loss(W_fc2) + tf.nn.l2_loss(b_fc2) +
-                        tf.nn.l2_loss(W_fc4) + tf.nn.l2_loss(b_fc4)
+        self.regularizers = (tf.nn.l2_loss(W_conv1) +
+                        tf.nn.l2_loss(W_conv2) +
+                        tf.nn.l2_loss(W_conv3) +
+                        tf.nn.l2_loss(W_conv4) +
+                        tf.nn.l2_loss(W_fc1) +
+                        tf.nn.l2_loss(W_fc2) +
+                        tf.nn.l2_loss(W_fc4)
                         )
         # Add the regularization term to the loss.
         # Add regression loss to cross entropy loss and regularizers. Arbitrary scalars to balance out the 3 things and give regression steering priority
         # self.squared_diff = tf.reduce_mean(tf.squared_difference(self.steering_regress_result, self.steering_regress_))
         # self.squared_diff = tf.squared_difference(self.steering_regress_result, self.steering_regress_)
-        cross_entropy = cross_entropy*1.0 + 0.0001 * regularizers + self.squared_diff*0.1 + self.squared_diff_throttle*0.1
-        # cross_entropy = 0.0001 * regularizers + self.squared_diff*0.1 + self.squared_diff_throttle*0.1
+        self.loss = 0.001 * self.regularizers + self.squared_diff*0.1 + self.squared_diff_throttle*5.0
+        tf.summary.scalar('loss', self.loss)
         # -----------------------------------------------------------------------------------
 
 
         # http://stackoverflow.com/questions/35298326/freeze-some-variables-scopes-in-tensorflow-stop-gradient-vs-passing-variables
         # ALEX-NET
-        # optimizerA = tf.train.MomentumOptimizer(1.0, 0.9)
         optimizerA = tf.train.AdamOptimizer(3e-4)
         main_train_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "shared_fc") + tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "shared_conv") + tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "main")
-        self.train_step = optimizerA.minimize(cross_entropy, var_list=main_train_vars)
-
-
-        # THIS NOW DOES A SUM OF SQUARES, NOT AN EXACT MATCH.
-        def compute_pred_accuracy(output, label):
-            prediction = tf.argmax(output, 1)
-            delta = (prediction - tf.argmax(label, 1))
-            energy = delta * delta
-            accuracy = tf.reduce_mean(tf.cast(energy, "float"))
-
-            return prediction, accuracy
-
-        self.steering_pred, self.steering_accuracy = compute_pred_accuracy(self.steering_softmax, self.steering_)
-        self.throttle_pred, self.throttle_accuracy = compute_pred_accuracy(self.throttle_softmax, self.throttle_)
-        # self.lat_pred, self.lat_accuracy = compute_pred_accuracy(self.lat_softmax, self.lat_)
-        # self.lon_pred, self.lon_accuracy = compute_pred_accuracy(self.lon_softmax, self.lon_)
-
-
-
-# width = 128
-# height = 128
-# widthD2 = width / 2
-# heightD2 = height / 2
-# widthD4 = width / 4
-# heightD4 = height / 4
-# widthD8 = width / 8
-# heightD8 = height / 8
-# widthD16 = width / 16
-# heightD16 = height / 16
-# max_log_outs = 15
-# img_channels = 3
-# fc1_num_outs = 256
-# l1_conv_size = 3
-# l1_num_convs = 8
-# l2_conv_size = 3
-# l2_num_convs = 12
-# l3_conv_size = 3
-# l3_num_convs = 16
-# l4_conv_size = 3
-# l4_num_convs = 32
-#
-# numPulses = 15
-# pulseScale = 64.0 #, 120.0
-#
-# h_pool5_odo_concat = None
-#
-#
-# def gen_graph_ops():
-#     global h_pool5_odo_concat
-#     x = tf.placeholder(tf.float32, shape=[None, width * height * img_channels], name='x')
-#     odo = tf.placeholder(tf.float32, shape=[None, 1], name='odo')
-#     vel = tf.placeholder(tf.float32, shape=[None, 1], name='vel')
-#     pulse = tf.placeholder(tf.float32, shape=[None, numPulses], name='pulse')
-#     steering_ = tf.placeholder(tf.float32, shape=[None, max_log_outs], name='steering_')
-#     throttle_ = tf.placeholder(tf.float32, shape=[None, max_log_outs], name='throttle_')
-#
-#     keep_prob = tf.placeholder(tf.float32)
-#     train_mode = tf.placeholder(tf.float32)
-#
-#     W_conv1 = weight_variable([l1_conv_size, l1_conv_size, img_channels, l1_num_convs], l1_conv_size * l1_conv_size * img_channels, l1_conv_size * l1_conv_size * l1_num_convs, name='W_conv1')
-#     b_conv1 = bias_variable([l1_num_convs])
-#     x_image = tf.reshape(x, [-1, width, height, img_channels])
-#     h_conv1 = tf.nn.relu(conv2d(x_image / 255.0 - 0.5, W_conv1) + b_conv1)
-#     h_pool1 = max_pool_2x2(h_conv1)#, name='h_pool1', collections='htmlize')
-#
-#     W_conv2 = weight_variable([l2_conv_size, l2_conv_size, l1_num_convs, l2_num_convs], l2_conv_size * l2_conv_size * l1_num_convs, l2_conv_size * l2_conv_size * l2_num_convs, name='W_conv2')
-#     b_conv2 = bias_variable([l2_num_convs])
-#     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-#     h_pool2 = max_pool_2x2(h_conv2)
-#
-#     W_conv3 = weight_variable([l3_conv_size, l3_conv_size, l2_num_convs, l3_num_convs], l3_conv_size * l3_conv_size * l2_num_convs, l3_conv_size * l3_conv_size * l3_num_convs, name='W_conv3')
-#     b_conv3 = bias_variable([l3_num_convs])
-#     h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
-#     h_pool3 = max_pool_2x2(h_conv3)
-#
-#     W_conv4 = weight_variable([l4_conv_size, l4_conv_size, l3_num_convs, l4_num_convs], l4_conv_size * l4_conv_size * l3_num_convs, l4_conv_size * l4_conv_size * l4_num_convs, name='W_conv4')
-#     b_conv4 = bias_variable([l4_num_convs])
-#     h_conv4 = tf.nn.relu(conv2d(h_pool3, W_conv4) + b_conv4)
-#     h_pool4 = max_pool_2x2(h_conv4)
-#     conv_maxes = tf.reduce_max(h_pool4, [1, 2])
-#     debug_layer = h_pool4
-#
-#     W_fc1 = weight_variable([widthD16 * heightD16 * l4_num_convs + 2 + numPulses, fc1_num_outs], widthD16 * heightD16 * l4_num_convs + 2 + numPulses, fc1_num_outs, name='W_fc1')
-#     b_fc1 = bias_variable([fc1_num_outs])
-#     h_pool5_flat = tf.reshape(h_pool4, [-1, widthD16 * heightD16 * l4_num_convs])
-#     # h_pool5_odo_concat = tf.concat(1, [h_pool5_flat, odo*0.0, vel, pulse*config.use_odometer])
-#     # Zero out odometer to experiment with odometer prediction.
-#     h_pool5_odo_concat = tf.concat(1, [h_pool5_flat, odo*0.0, vel, pulse*0.0])
-#
-#     h_fc1 = tf.nn.relu(tf.matmul(h_pool5_odo_concat, W_fc1) + b_fc1)
-#     h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-#
-#     num_outputs = 2 * max_log_outs
-#     num_outputs += numPulses  # Add numpulses for odometer prediction.
-#     W_fc4 = weight_variable([fc1_num_outs, num_outputs], fc1_num_outs, num_outputs, name='W_fc4')
-#     b_fc4 = bias_variable([num_outputs])
-#
-#     output = (tf.matmul(h_fc1_drop, W_fc4) + b_fc4)
-#     steering_softmax = tf.nn.softmax(output[:, :max_log_outs])
-#     throttle_softmax = tf.nn.softmax(output[:, max_log_outs:max_log_outs + max_log_outs])  # for version without odometer prediction: output[:, max_log_outs:]
-#     pulse_softmax = tf.nn.softmax(output[:, max_log_outs + max_log_outs:])  # odometer prediction
-#
-#     # cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
-#     # http://stackoverflow.com/questions/33712178/tensorflow-nan-bug
-#
-#     steering_cross_entropy = -tf.reduce_mean(steering_ * tf.log(tf.clip_by_value(steering_softmax, 1e-10, 1.0)))
-#     throttle_cross_entropy = -tf.reduce_mean(throttle_ * tf.log(tf.clip_by_value(throttle_softmax, 1e-10, 1.0)))
-#     pulse_cross_entropy = -tf.reduce_mean(pulse * tf.log(tf.clip_by_value(pulse_softmax, 1e-10, 1.0)))
-#     # cross_entropy = (steering_cross_entropy + throttle_cross_entropy) / 2.
-#     # cross_entropy = ((steering_cross_entropy + throttle_cross_entropy) * train_mode + pulse_cross_entropy*0.6) / (0.6 + train_mode * 2.0)
-#     cross_entropy = (steering_cross_entropy + throttle_cross_entropy*0.3 + pulse_cross_entropy*0.3) / 1.6
-#     # L2 regularization
-#     regularizers = (tf.nn.l2_loss(W_conv1) + tf.nn.l2_loss(b_conv1) +
-#                     tf.nn.l2_loss(W_conv2) + tf.nn.l2_loss(b_conv2) +
-#                     tf.nn.l2_loss(W_conv3) + tf.nn.l2_loss(b_conv3) +
-#                     tf.nn.l2_loss(W_conv4) + tf.nn.l2_loss(b_conv4) +
-#                     tf.nn.l2_loss(W_fc1) + tf.nn.l2_loss(b_fc1) +
-#                     tf.nn.l2_loss(W_fc4) + tf.nn.l2_loss(b_fc4)
-#                     )
-#     # Add the regularization term to the loss.
-#     cross_entropy += 0.00002 * regularizers # 5e-4
-#     train_step = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy) # 1e-4
-#     # train_step = tf.train.GradientDescentOptimizer(0.00001).minimize(cross_entropy) # for 3-conv 0.000001
-#     # train_step = tf.train.AdagradOptimizer(0.0002).minimize(cross_entropy) # for 3-conv 0.001
-#     # train_step = tf.train.MomentumOptimizer(0.00001, 0.9).minimize(cross_entropy)
-#
-#     # THIS NOW DOES A SUM OF SQUARES, NOT AN EXACT MATCH.
-#     def compute_pred_accuracy(output, label):
-#         prediction = tf.argmax(output, 1)
-#         delta = (prediction - tf.argmax(label, 1))
-#         energy = delta * delta
-#         accuracy = tf.reduce_mean(tf.cast(energy, "float")) * 0.1  # arbitrary scale
-#         # correctly_predicted = tf.equal(tf.argmax(label, 1), prediction)
-#         # accuracy = tf.reduce_mean(tf.cast(correctly_predicted, "float"))
-#
-#         return prediction, accuracy
-#
-#     steering_pred, steering_accuracy = compute_pred_accuracy(steering_softmax, steering_)
-#     throttle_pred, throttle_accuracy = compute_pred_accuracy(throttle_softmax, throttle_)
-#
-#     return x, odo, vel, pulse, steering_, throttle_, keep_prob, train_mode, train_step, steering_pred, steering_accuracy, throttle_pred, throttle_accuracy, steering_softmax, throttle_softmax, pulse_softmax, conv_maxes, debug_layer
+        self.train_step = optimizerA.minimize(self.loss, var_list=main_train_vars)

NeuralNet/html_output.py
@@ -145,20 +145,18 @@ def draw_softmax_distribution(outfile, label, softmax, gt, draw_zero_point=False
     encode_image_as_html(outfile, soft_img, 'PNG', 'style="position:absolute"')
     outfile.write(label + '</div><br/>')
 
-def write_html_image(outfile, result_steering, result_throttle, test_data, w, h, message, im_id, steering_softmax, throttle_softmax, steering_regress, throttle_regress):
+def write_html_image(outfile, test_data, w, h, message, im_id, steering_regress, throttle_regress):
     images = test_data.pic_array[im_id]
-    answers = test_data.steer_onehot_array[im_id]
-    answers_throttle = test_data.throttle_onehot_array[im_id]
-    steering_gt = argmax(answers)
-    throttle_gt = argmax(answers_throttle)
+    steer_gt = test_data.steer_array[im_id]
+    throttle_gt = test_data.throttle_array[im_id]
 
     # pulse_entropy = np.sum( np.multiply(pulse_softmax, np.log(np.reciprocal(pulse_softmax))) )
 
     # Fade color from green to yellow to red and make a table cell with that background color.
     # (40, 88, 136, 184, 232, 280)
     # (400,352,304, 256, 208, 160, 112, 64, 16)
-    delta = abs(steering_gt - result_steering)
-    shade = 'rgb(%s, %s, %s)' % (min(232, delta * 52 + 20), max(0, min(255-32, 448 - delta * 48)), min(80, delta * 80))
+    # delta = abs(steering_gt - result_steering)
+    # shade = 'rgb(%s, %s, %s)' % (min(232, delta * 52 + 20), max(0, min(255-32, 448 - delta * 48)), min(80, delta * 80))
     #remap entropy to around [0..1] range.
     # ent01 = min(1.0, max(0.0, pulse_entropy * 0.5 - 0.1))
     # shade = 'rgb(%s, %s, %s)' % (min(255,int(ent01 * 255*2)), max(0,int((2.0-ent01*2.0) * 255)), 64)
@@ -177,45 +175,20 @@ def write_html_image(outfile, result_steering, result_throttle, test_data, w, h,
     #write_html_image_tensor_gray_overlay(outfile, latlon_softmax.reshape((15, 15))*255, 9, "", "", 1.0)
     #outfile.write('<svg height="128" width="128" style="position:absolute;top:2px;left:138px"><circle cx="' + str(gt_lon * 128 / 15 + 7) + '" cy="' + str(gt_lat * 128 / 15 + 7) + '" r="4" stroke="black" stroke-width="1" fill="yellow" /></svg>')
     write_steering_line(outfile, -steering_regress * 1, 'rgb(240, 55, 40)', 7)
-    write_steering_line(outfile, -(steering_gt - 7) * 7, 'rgb(40, 255, 40)', 5)
-    write_steering_line(outfile, -(result_steering - 7) * 7)
-    # write_vertical_meter(outfile, throttle_gt, total, 'rgb(40, 255, 40)')
-    # write_vertical_meter(outfile, throttle_net, total)
+    write_vertical_meter(outfile, throttle_gt, 16, 'rgb(40, 255, 40)')
+    write_vertical_meter(outfile, throttle_regress, 16)
     outfile.write('</span>')
 
-    # Draw *steering* softmax distribution to a png
-    draw_softmax_distribution(outfile, 'Steer softmax', steering_softmax, steering_gt)
-
-    # Draw *throttle* softmax distribution to a png
-    draw_softmax_distribution(outfile, 'Throttle softmax', throttle_softmax, throttle_gt, True)
-
-    # Draw *pulse* softmax distribution to a png
-    # soft_img = Image.new('RGBA', (128, 32), (0, 0, 0, 64))
-    # draw = ImageDraw.Draw(soft_img)
-    # soft_size = pulse_softmax.shape[0]
-    # scale = (soft_img.width / soft_size)
-    # for i in range(soft_size):
-    #     fill_color = (255, 255, 255, 255)
-    #     # if i == throttle_gt: fill_color = (0, 255, 0, 255)
-    #     prob = pulse_softmax[i]
-    #     draw.rectangle(
-    #         [i * scale, soft_img.height - round(prob * soft_img.height), i * scale + scale - 2, soft_img.height],
-    #         fill_color)
-    # outfile.write('<div style="position:relative;padding-bottom:4px">')
-    # encode_image_as_html(outfile, soft_img, 'PNG', 'style="position:absolute"')
-    # outfile.write('Odometer softmax</div><br/>')
-
     # Print out throttle, steering, and odometer values.
-    outfile.write('T: ' + str(throttle_gt) + '&nbsp;&nbsp; NT: ' + str(result_throttle) + '<br/>')
-    outfile.write('S: ' + str(steering_gt) + '&nbsp;&nbsp; NS: ' + str(result_steering) + '<br/>')
-    outfile.write('regress: ' + str(int(steering_regress)) + ' ' + str(int(throttle_regress)) + '<br/>')
+    outfile.write('<br/>regress: ' + str(int(steering_regress)) + ' ' + str(int(throttle_regress)) + '<br/>')
+    outfile.write('gt: ' + str(int(steer_gt)) + ' ' + str(int(throttle_gt)) + '<br/>')
     # outfile.write('ent: ' + str(pulse_entropy))
     outfile.write('</td>')
 
 
-def write_html(output_path, test_data, results_steering, results_throttle, graph, sess, steering_softmax_batch, throttle_softmax_batch, results_steering_regress, results_throttle_regress, net_model):
+def write_html(output_path, test_data, graph, sess, results_steering_regress, results_throttle_regress, net_model):
     # copyfile("track_extents_white.png", os.path.join(output_path, "track_extents_white.png"))
-    image_count = len(results_steering)
+    image_count = len(results_steering_regress)
     image_count = min(1000, image_count)
     outfile = open(os.path.join(output_path, "debug.html"), "w")
     outfile.write("""
@@ -286,15 +259,15 @@ def write_html(output_path, test_data, results_steering, results_throttle, graph
         if (i % 16) == 0:
             outfile.write('</tr>')
             outfile.write('<tr>')
-        write_html_image(outfile, results_steering[i], results_throttle[i], test_data, net_model.width, net_model.height, "blank", i,
-                         steering_softmax_batch[i], throttle_softmax_batch[i], results_steering_regress[i], results_throttle_regress[i])
+        write_html_image(outfile, test_data, net_model.width, net_model.height, "blank", i,
+                         results_steering_regress[i], results_throttle_regress[i])
     outfile.write('</tr>')
     outfile.write('</table><br/><br/>')
 
     outfile.write("<div style='position:relative'>")
     write_html_image(
-        outfile, results_steering[0], results_throttle[0], test_data, net_model.width, net_model.height, "blank2", 0,
-        steering_softmax_batch[0], throttle_softmax_batch[0], results_steering_regress[0], results_throttle_regress[0])
+        outfile, test_data, net_model.width, net_model.height, "blank2", 0,
+        results_steering_regress[0], results_throttle_regress[0])
     outfile.write('</div>')
     # write_html_image_RGB(outfile, all_xs[0], width, height)
     # viz = sess.run(W)