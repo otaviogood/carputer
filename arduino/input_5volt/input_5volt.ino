868c21bd4f8bba7d65c3a235ead7af603b538b43
Gilbert Montague <gilbert.i.montague@gmail.com>
2018-05-12 23:41:31 -0700

    Updated throttle command

--- arduino/input_5volt/input_5volt.ino
+++ arduino/input_5volt/input_5volt.ino
@@ -31,18 +31,19 @@ void loop() {
 
   // Observed ranges:
   // - steering: Left = 1070, center = 1401, Left = 1733
-  // - throttle: min = 1010, center = 1465, max = 1975
+  // - throttle: min = 1059, center = 1503, max = 1948
 
   // Deliberately use less precision to avoid 16-bit overflow
   const int loss = 5;
 
   // Map input to: min = 0, center = 90, max = 180
-  steering = clamp(90 + (steering - 1401) * (90 / loss) / (331 / loss), 0, 180);
-//  throttle = clamp(90 + (throttle - 1465) * (90 / loss) / (455 / loss), 0, 180);
+  //steering = clamp(90 + (steering - 1401) * (90 / loss) / (331 / loss), 0, 180);
+  throttle = clamp(90 + (throttle - 1503) * (90 / loss) / (445 / loss), 0, 180);
+  throttle = map(throttle, 180, 0, 0, 180);
 
-  Serial.println(steering);
+  //Serial.println(steering);
   //Serial.print(" ");
-  //Serial.print(throttle);
+  Serial.println(throttle);
   //Serial.print(" ");
   //Serial.println(remote_button);
   // delay for good luck. pulseIn should have already delayed
